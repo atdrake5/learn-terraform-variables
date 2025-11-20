11f8f9332dd7564372c9f332b2df2ad4453e3352
TEK Student <TEKstudent@vlab.lan>
2025-11-20 06:02:31 -0800

    updated bucket_name variable

--- variables.tf
+++ variables.tf
@@ -8,3 +8,9 @@ variable "instance_count" {
   description = "Number of EC2 instances to create"
   type        = number
 }
+
+variable "bucket_name" {
+  description = "my bucket"
+  type = string
+  default = "s3-bucket-atd"
+}
